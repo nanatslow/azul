f25f1d23ed165f0453d4cd925320e54b4be4650e
Natália <141193400+nanatslow@users.noreply.github.com>
2023-08-14 20:15:59 -0300

    Add files via upload

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,34 @@
+
+
+package aula04;
+
+/**
+ * Exercício 04
+ * @author Natália Felipe 
+ */
+public class Aula04 {
+    public static void main(String[] args) {
+        int a =10;
+                String b = "10";
+                int c = 2;
+               
+                
+                int resultadoA = a + c;
+                int resultadoB = a - c;
+                int resultadoC = a * c;
+                int resultadoD = a / c;
+                int resultadoE = a % c;
+                int resultadoF = (int)a / c;
+                
+                System.out.println (resultadoA);
+                System.out.println (resultadoB);
+                System.out.println (resultadoC);
+                System.out.println (resultadoD);
+                System.out.println (resultadoE);
+                System.out.println (resultadoF);
+               
+ 
+                                
+    }
+    
+}
