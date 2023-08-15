0937d47266b503d25af224aa51476aebba55d5e3
Natália <141193400+nanatslow@users.noreply.github.com>
2023-08-15 19:21:43 -0300

    Add files via upload

---
+++ aula05/aula05.java
@@ -0,0 +1,22 @@
+
+package aula05;
+
+/**
+ *
+ * @author Natália Felipe
+ */
+public class aula05 {
+    public static void main (String[] args){
+        System.out.println ("O valor do pi é:"+ Math.PI);
+        System.out.println ("Maximo:"+ Math.max(5, 10));
+        System.out.println ("Minimo:"+ Math.min(5, 10) );
+        System.out.println ("Raiz Quadrada:"+ Math.sqrt(64));
+        
+        System.out.println ("Exponencial e potenciação \n");
+        System.out.println ("O valor do E é:"+ Math.E + "\n\n");
+        System.out.println ("'e' elevado ao quadrado = "+ Math.exp(2));
+        System.out.println ("2 elevado"+ Math.pow(2, 3)+ "\n\n");
+        
+    }
+    
+}
---
+++ aulaex06/aula06.java
@@ -0,0 +1,24 @@
+
+package aulaex06;
+
+/**
+ *
+ * @author 70463550617
+ */
+public class aula06 {
+    public static void main(String[] args) {
+        float nota1 = 8;
+        float nota2 = 5;
+        float nota3 = 7;
+        float nota4 = 4;
+        
+        float media, soma = 0;
+        
+        
+       soma = nota1 + nota2 + nota3 + nota4;
+       media = soma / 4;
+       System.out.println ("Soma: "+ soma);
+       System.out.println ("Media das notas:"+ media);
+        
+    }
+}
