620b5be640e822681dd52f014e15a3625960d09d
Natália <141193400+nanatslow@users.noreply.github.com>
2023-08-29 21:54:40 -0300

    Add files via upload

---
+++ aula16/usuario.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula16;
+
+/**
+ *
+ * @author 70463550617
+ */
+public class usuario {
+    private String nome = "natalia";
+    private String senha = "1234";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
